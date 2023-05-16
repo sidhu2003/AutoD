deb120ade4f00054f1815bf8f591238befbcb92a
Venkata Siddardha Rali <68889220+sidhu2003@users.noreply.github.com>
2023-05-16 19:18:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
-
+#!/bin/sh 
+#author : Venkata Siddardha Rali
+sudo apt-get update
+sudo apt-get install apache2
+sudo apt install unzip
+wget <your github repo zip file link>
+sudo unzip main.zip #make sure you download main branch
+sudo cp <repo name>-main/* /var/www/html 
