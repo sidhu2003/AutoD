165eefd94978b025fb994a27436213b9dc3c06f1
Venkata Siddardha Rali <68889220+sidhu2003@users.noreply.github.com>
2024-02-05 18:18:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 sudo apt-get update
 sudo apt-get install apache2
 sudo apt install unzip
-wget <your github repo zip file link>
-sudo unzip main.zip #make sure you download main branch
-sudo cp -r <repo name>-main/* /var/www/html 
+wget $1
+sudo unzip $2 #your branch.zip
+sudo cp -r $3/* /var/www/html #your <repo name>-<branch> 
